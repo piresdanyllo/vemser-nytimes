b5e5b24cc40537c64dadfa1f4a4d5d5995cf5854
Danyllo Pires <pires.danyllo@yahoo.com.br>
2022-07-24 16:05:43 -0300

    renderizando a noticia

--- src/pages/News/News.js
+++ src/pages/News/News.js
@@ -1,20 +1,48 @@
-import HeaderSection from '../../components/Header/HeaderSection/HeaderSection'
-import styles from './News.module.css'
+import axios from "axios";
+import { useState, useEffect } from "react";
+import HeaderSection from "../../components/Header/HeaderSection/HeaderSection";
+import styles from "./News.module.css";
 
 const News = () => {
+  let path = window.location.pathname;
+  let pathSplit = path.split("/");
+  let identificador = parseInt(pathSplit[2]);
+  let section = pathSplit[3];
+
+  const [id, setId] = useState('')
+
+  const [news, setNews] = useState([]);
+
+  const setup = async () => {
+    try {
+      const { data } = await axios.get(
+        `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=hneVGzao1mNHHI7NL2dGC9Rax0EULZR5`
+      );
+      setNews(data.results);
+      setId(identificador)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    setup();
+  }, []);
+
   return (
     <>
-    <HeaderSection/>
-    <div className={styles.newsContainer}>
-        <h2>kicker</h2>
-        <h1>title</h1>
-        <p>abstract</p>
-        <img src="" alt="" />
-        <small>caption copyright</small>
-        <p>abstract</p>
-    </div>
+      <HeaderSection />
+      {news.slice(id, id+1).map((item ,i) => (
+      <div key={i} className={styles.newsContainer}>
+        <h1>{item.title}</h1>
+        <p>{item.abstract}</p>
+        <img src={item.multimedia[1].url} alt="" />
+        <small>{item.caption} {item.copyright}</small>
+        <p>{item.abstract}</p>
+      </div>
+      ))}      
     </>
-  )
-}
+  );
+};
 
-export default News
+export default News;
